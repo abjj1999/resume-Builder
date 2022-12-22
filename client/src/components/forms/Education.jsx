ae5fb2b78a79e364253cca12b829a2e8d001b749
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-12-21 23:47:05 -0600

    created and 50% finished eduction page

--- client/src/components/forms/Education.jsx
+++ client/src/components/forms/Education.jsx
@@ -1,7 +1,117 @@
 import React from 'react'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
+import { Button, Form, Input, Space } from 'antd'
 
 const Education = () => {
-  return <div>Education</div>
+  const onFinish = (values) => {
+    console.log('Received values of form:', values)
+  }
+  return (
+    <div>
+      <h4 className="m-2 text-center mb-4">Education</h4>
+      <Form
+        name="dynamic_form_nest_item"
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.List name="education">
+          {(fields, { add, remove }) => (
+            <div className="border p-3 m-3">
+              {fields.map(({ key, name, ...restField }) => (
+                <div className="">
+                  <Space
+                    key={key}
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      flexWrap: 'wrap',
+                    }}
+                  >
+                    <Form.Item
+                      {...restField}
+                      label="School Name"
+                      name={[name, 'School']}
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Missing School Name',
+                        },
+                      ]}
+                    >
+                      <Input placeholder="School " />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'degree']}
+                      label="Degree"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Missing Degree',
+                        },
+                      ]}
+                    >
+                      <Input placeholder="Degree" />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'date']}
+                      label="Date"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Missing Date',
+                        },
+                      ]}
+                    >
+                      <Input placeholder="Date" />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'GPA']}
+                      label="GPA"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Missing GPA',
+                        },
+                      ]}
+                    >
+                      <Input className="w-50" placeholder="GPA" />
+                    </Form.Item>
+                    <Form.Item>
+                      <MinusCircleOutlined
+                        className="fs-5"
+                        onClick={() => remove(name)}
+                      />
+                    </Form.Item>
+                  </Space>
+                </div>
+              ))}
+
+              <Form.Item className="d-flex justify-content-center">
+                <Button
+                  type="dashed"
+                  onClick={() => add()}
+                  block
+                  icon={<PlusOutlined />}
+                >
+                  Add field
+                </Button>
+              </Form.Item>
+            </div>
+          )}
+        </Form.List>
+        {/* add skills here  */}
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
 
 export default Education
--- client/src/components/forms/PersonalInfo.jsx
+++ client/src/components/forms/PersonalInfo.jsx
@@ -1,11 +1,15 @@
 import { Input } from 'antd'
 import React from 'react'
 import { AiOutlineClear, AiOutlineSave } from 'react-icons/ai'
+import { Form } from 'antd'
 
 const { TextArea } = Input
 const PersonalInfo = () => {
+  const personalInfo = (values) => {
+    console.log(values)
+  }
   return (
-    <div className="h-100">
+    <Form onFinish={personalInfo} className="h-100">
       <div className="d-flex justify-content-center flex-column flex-wrap align-items-center">
         <h4 className="m-2 text-center mb-4">Personal Info</h4>
       </div>
@@ -14,23 +18,27 @@ const PersonalInfo = () => {
           <label htmlFor="firstname">
             First name <span className="text-danger">*</span>
           </label>
-          <Input
-            type="text"
-            className="form-control"
-            id="firstname"
-            placeholder="First name"
-          />
+          <Form.Item name="FirstName">
+            <Input
+              type="text"
+              className="form-control"
+              id="firstname"
+              placeholder="First name"
+            />
+          </Form.Item>
         </div>
         <div className="mb-2 form-group col-lg-4 col-md-5 col-sm-10">
           <label htmlFor="lastname">
             Last name <span className="text-danger">*</span>
           </label>
-          <Input
-            type="text"
-            className="form-control"
-            id="lastname"
-            placeholder="Last name"
-          />
+          <Form.Item name="LastName">
+            <Input
+              type="text"
+              className="form-control"
+              id="lastname"
+              placeholder="Last name"
+            />
+          </Form.Item>
         </div>
       </div>
       <div className=" mt-3 row d-flex justify-content-center">
@@ -39,41 +47,50 @@ const PersonalInfo = () => {
           <label htmlFor="email">
             Email <span className="text-danger">*</span>
           </label>
-          <Input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="email@example.com"
-          />
+          <Form.Item name="Email">
+            <Input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="email@example.com"
+            />
+          </Form.Item>
         </div>
 
         <div className="mb-2 form-group col-lg-3 col-md-5 col-sm-10">
           <label htmlFor="portfolio">
             Portfolio <span className="text-muted">(optional)</span>
           </label>
-          <Input
-            type="url"
-            className="form-control "
-            id="portfolio"
-            placeholder="www.example.com"
-          />
+          <Form.Item name="Portfolio">
+            <Input
+              type="url"
+              required={false}
+              className="form-control "
+              id="portfolio"
+              placeholder="www.example.com"
+            />
+          </Form.Item>
         </div>
         <div className="mb-2 form-group col-lg-2 col-md-5 col-sm-10">
           <label htmlFor="phone">
             Phone <span className="text-danger">*</span>
           </label>
-          <Input
-            type="text"
-            className="form-control "
-            id="phone"
-            placeholder="(xxx) xxx-xxxx"
-          />
+          <Form.Item name="Phone">
+            <Input
+              type="text"
+              className="form-control "
+              id="phone"
+              placeholder="(xxx) xxx-xxxx"
+            />
+          </Form.Item>
         </div>
       </div>
       <div className=" mt-3 row d-flex justify-content-center">
         <h6 className="text-center">Objective</h6>
         <div className="mb-2 form-group col-8">
-          <TextArea placeholder="Write a summery" rows={3} />
+          <Form.Item name="Objective">
+            <TextArea placeholder="Write a summery" rows={3} />
+          </Form.Item>
         </div>
       </div>
       <div className=" mt-3 row d-flex justify-content-center">
@@ -82,38 +99,38 @@ const PersonalInfo = () => {
           <label htmlFor="City">
             City <span className="text-danger">*</span>
           </label>
-          <Input
-            type="text"
-            className="form-control"
-            id="City"
-            placeholder="City"
-          />
+          <Form.Item name="City">
+            <Input
+              type="text"
+              className="form-control"
+              id="City"
+              placeholder="City"
+            />
+          </Form.Item>
         </div>
         <div className="mb-2 form-group col-lg-3 col-md-5 col-sm-10">
           <label htmlFor="State">
             State <span className="text-danger">*</span>
           </label>
-          <Input
-            type="text"
-            className="form-control"
-            id="State"
-            placeholder="State"
-          />
+          <Form.Item name="State">
+            <Input
+              type="text"
+              className="form-control"
+              id="State"
+              placeholder="State"
+            />
+          </Form.Item>
         </div>
       </div>
-      <div className=" d-flex m-4  justify-content-center ">
-        <div className="  d-flex">
-          <div className=" point m-3 d-flex flex-column">
-            <AiOutlineSave className="m-1 text-primary fs-4" />
-            <p className="text-center">Save</p>
-          </div>
-          <div className=" point m-3 d-flex flex-column">
-            <AiOutlineClear className="m-1 text-danger fs-4" />
-            <p className="text-center">Clear</p>
-          </div>
-        </div>
+      <div className=" d-flex   justify-content-center ">
+        <button type="submit" className="btn btn-primary point m-2  d-flex">
+          <AiOutlineSave className=" m-1 fs-4 " />
+        </button>
+        <button className=" btn btn-danger  d-flex m-2 ">
+          <AiOutlineClear className="m-1  fs-4" />
+        </button>
       </div>
-    </div>
+    </Form>
   )
 }
 
